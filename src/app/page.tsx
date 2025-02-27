2841f56b2aae22f2afc032c12d9cc51de77bf5f3
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-26 19:37:35 -0500

    about me doing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,14 +13,16 @@ const more_link : string = "www.google.com";
 const description : string = "I am Abhishek Prakash (Abhi), a professional with over two years of industry experience and a master's degree in computer science from <a href='https://www.fsu.edu' target='_blank' style='color: #CEB888; text-decoration: none;' onmouseover='this.style.color = \"#782F40\"' onmouseout='this.style.color=\"#CEB888\"'>Florida State University</a>. My expertise lies in various programming languages, with a strong emphasis on Python. I specialize in machine learning, automation, software development, and the design and deployment of cloud infrastructure as code. I am proficient in popular machine learning and deep learning frameworks such as PyTorch, TensorFlow, scikit-learn, and pandas. My comprehensive background in computer science, including an in-depth understanding of data structures and algorithms, underpins my professional journey. Throughout my career, I have been actively involved in a wide range of software projects, from developing small-scale tools to orchestrating large-scale production systems. My experience extends to website development, including front-end, back-end, and database integration, ensuring the robustness and efficiency of solutions through unit testing, data visualization, and data engineering.In addition to my core skills, I have specialized in Machine Learning Operations (MLOps), managing CI/CD pipelines and Docker images to ensure the seamless integration and deployment of machine learning models. I am adept at using automation tools like Ansible and Terraform, streamlining and automating MLOps processes to enhance the overall efficiency and reliability of machine learning workflows.";
 const mark_down_data : string  = " ## Heading\n The classic example that we can understand dynamic programming with is the Fibonacci series, which can be solved both recursively and using dynamic programming.\n ### Recursive Approach \n ```python \ndef fibbo(nums):\n      if nums <= 1:\n        return 1\n      return fibbo(n-1) + fibbo(n-2)\n ```\n\n### Dynamic Programming with Memoization\n ```python\ndef fibonacci(n, memo={}):\n       if n in memo:\n         return memo[n]\n       if n <= 1:\n         return n\n       else:\n          memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)\n       return memo[n]\n ```\n\n### Dynamic Programming with Tabulation\n ```python\ndef fibonacci(n):\n        if n <= 1:\n            return n\n   \n        fib_table = [0] * (n + 1)\n        fib_table[1] = 1\n\n        for i in range(2, n + 1):\n           fib_table[i] = fib_table[i - 1] + fib_table[i - 2]\n\n        return fib_table[n]\n"
 const socialLinks = [
-  "https://github.com/yourprofile",
-  "https://www.linkedin.com/in/yourprofile/",
+  "https://github.com/abhishekprakash256",
+  "https://www.linkedin.com/in/abhishek256/",
   "https://twitter.com/yourprofile",
-  "https://leetcode.com/yourprofile",
-  "https://gitlab.com/yourprofile",
-  "https://www.kaggle.com/yourprofile",
+  "https://leetcode.com/abhishekprakash256/",
+  "https://gitlab.com/abhishekprakash256",
+  "https://www.kaggle.com/abhishek256",
   "https://medium.com/@yourprofile",
 ];
+
+
 const article_image : string = "chat-app-icon.png";
 
 const profile_pic : string = "profile-pic.jpg";
@@ -308,15 +310,8 @@ export default function Home() {
 
 
 
-<HeadingBar title={"Abhi's Microcom in page"}/> 
-<div>
-      <h1>Welcome to the Home Page</h1>
-      <p>
-        Go to the <Link href="/about">About Page</Link>
-      </p>
-    </div>
+<HeadingBar title={"Abhi's Microcosm"}/> 
 
-    <More more_link= "http://localhost:3000/about" /> 
 
 <AboutPic image_link={profile_pic} />
 
@@ -329,6 +324,7 @@ export default function Home() {
   kaggle_link={socialLinks[5]}
   medium_link=""
 />
+
 
 <Para description={ description }/>
 
