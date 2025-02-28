67dde8c63dfab6b509b4f4e42260505924744552
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-02-28 11:16:28 -0500

    search working started

---
+++ src/app/search/[slug]/page.tsx
@@ -0,0 +1,27 @@
+/*
+The search page for displaying the search reusult in card format
+also display a page if any result is not avaialble as well 
+
+*/
+
+
+import {ButtonBar , CardLists, AboutPic, CardsPaignation , NavBar, Footer,ArticleImage, SpaceBlock, SocialMediaLinks, Para, MarkDown, HeadingBar,CustomBody, More } from "front-end-component-kit";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {CardData, ArticleData, SectionData } from "../../types"; // Import types
+
+
+const socialLinks = [
+    "https://github.com/abhishekprakash256",
+    "https://www.linkedin.com/in/abhishek256/",
+    "",
+    "https://leetcode.com/abhishekprakash256/",
+    "https://gitlab.com/abhishekprakash256",
+    "https://www.kaggle.com/abhishek256",
+    "",
+  ];
+  
+  const resume_link : string = "Resume.pdf";
+
+
+
+  
