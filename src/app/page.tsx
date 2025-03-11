1e8596a0aa57d0416556f5528e8106dcc2e8264a
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-03-11 15:34:42 -0400

    images organised

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ const socialLinks = [
 ];
 
 const description : string = "I am Abhishek Prakash (Abhi), a professional with over two years of industry experience and a master's degree in computer science from <a href='https://www.fsu.edu' target='_blank' style='color: #CEB888; text-decoration: none;' onmouseover='this.style.color = \"#782F40\"' onmouseout='this.style.color=\"#CEB888\"'>Florida State University</a>. My expertise lies in various programming languages, with a strong emphasis on Python. I specialize in machine learning, automation, software development, and the design and deployment of cloud infrastructure as code. I am proficient in popular machine learning and deep learning frameworks such as PyTorch, TensorFlow, scikit-learn, and pandas. My comprehensive background in computer science, including an in-depth understanding of data structures and algorithms, underpins my professional journey. Throughout my career, I have been actively involved in a wide range of software projects, from developing small-scale tools to orchestrating large-scale production systems. My experience extends to website development, including front-end, back-end, and database integration, ensuring the robustness and efficiency of solutions through unit testing, data visualization, and data engineering.In addition to my core skills, I have specialized in Machine Learning Operations (MLOps), managing CI/CD pipelines and Docker images to ensure the seamless integration and deployment of machine learning models. I am adept at using automation tools like Ansible and Terraform, streamlining and automating MLOps processes to enhance the overall efficiency and reliability of machine learning workflows.";
-const profile_pic : string = "./images/profile-pic.jpg";
+const profile_pic : string = "./images/profile-pic/profile-pic.jpg";
 const resume_link : string = "/resume.pdf";
 
 
@@ -41,7 +41,7 @@ const Paignation_data = [
   {
     card_title: "Card Title 2",
     card_para: "System design is a multidisciplinary field that encompasses various aspects of designing distributed systems.",
-    img_src: "./images/federated-learning-flow.png", /* the image file is in the public folder */
+    img_src: "./images/section/project/federated-learning/federated-learning-flow.png", /* the image file is in the public folder */
     card_url: "https://example.com/card2",
   },
   {
@@ -72,7 +72,7 @@ const Paignation_data = [
   {
     card_title: "Card Title 1",
     card_para: "System design is a multidisciplinary field that encompasses various aspects of designing distributed systems. ",
-    img_src: "./images/chat-app-icon.png",  /* the image file is in the public folder */
+    img_src: "./images/section/project/chat-app/chat-app-icon.png",  /* the image file is in the public folder */
     card_url: "https://example.com/card1", 
   },
   {
@@ -108,7 +108,7 @@ const Paignation_data = [
   {
     card_title: "Card Title 1",
     card_para: "System design is a multidisciplinary field that encompasses various aspects of designing distributed systems. ",
-    img_src: "./images/chat-app-icon.png",  /* the image file is in the public folder */
+    img_src: "./images/section/project/chat-app/chat-app-icon.png",  /* the image file is in the public folder */
     card_url: "https://example.com/card1", 
   },
   {
