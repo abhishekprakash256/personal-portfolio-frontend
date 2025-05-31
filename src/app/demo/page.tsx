aba46fada3ae3168cb691e5803a68949ee9bac5f
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-05-31 12:18:05 -0400

    fetch api test

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,79 @@
+ /*
+The testiing page for the url fetch requets using frtehc api 
+*/
+
+
+'use client';   // the use of client is with react componenent
+import { NavBar, HeadingBar, CustomBody } from "front-end-component-kit";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState, useEffect } from 'react';
+
+
+
+
+
+async function submitData()
+
+{
+    const dummy_data = { url: "https://www.google.com/" }
+
+    try  {
+
+        const response = await fetch("http://localhost:5050/tu/submit" , 
+        {
+            method : 'POST',
+            headers : {
+                'Content-Type': 'application/json',
+            },
+            
+            body: JSON.stringify(dummy_data) ,
+        });
+
+
+    if (!response.ok) {
+
+        throw new Error(`HTTP error status", ${response.ok}`)
+
+    }
+
+    const responseData = await response.json();
+
+    console.log('Success:', responseData); 
+
+   }
+
+    catch(error) {
+
+      console.error('Error:', error);
+   }
+}
+
+
+
+
+
+
+
+export default async function()
+{
+    const data = await fetch('https://api.vercel.app/blog')
+    const posts = await data.json()
+    const data_url = await submitData() ; 
+    return (
+      <ul>
+        
+        
+        {posts.map((post : any) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+    )
+}
+
+
+
+
+
+
+
+
