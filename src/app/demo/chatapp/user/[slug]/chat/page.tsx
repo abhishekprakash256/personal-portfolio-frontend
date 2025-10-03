7c93406ff07c65ed5399af3ec976add5ec35a94d
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-10-03 19:18:28 -0400

    chat page

--- src/app/demo/chatapp/user/[slug]/chat/page.tsx
+++ src/app/demo/chatapp/user/[slug]/chat/page.tsx
@@ -1,6 +1,23 @@
 /*
 The chat app page for the user
+
+flow --> 
+
+the chat page checks the session storage data 3 data --> chathash , sender and reciver
+
+if the data is found 
+check the data with the backend server 
+    -> if the data is correct connect to the ws server 
+    -> establish the connection
+
+if the data not found or data is wrong 
+return to the login page 
+
+
+
 */
+
+
 
 
 export default function Page() {
