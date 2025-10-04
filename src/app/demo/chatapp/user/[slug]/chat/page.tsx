fb812c8acd537ef98e3f43ef23fec1c9b7aee5fd
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-10-03 23:31:18 -0400

    chat page

--- src/app/demo/chatapp/user/[slug]/chat/page.tsx
+++ src/app/demo/chatapp/user/[slug]/chat/page.tsx
@@ -49,6 +49,11 @@ export default function ChatServerChat() {
     // Otherwise check login
     const loginCheck = async () => {
       try {
+
+        // dev http://localhost:8080/chat-server/user/login    Linux 
+        // dev  http://127.0.0.1:8080/chat-server/user/login   Mac 
+        // prod  http://meabhi.me/chat-server/user/login
+
         const response = await fetch("http://127.0.0.1:8080/chat-server/user/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
