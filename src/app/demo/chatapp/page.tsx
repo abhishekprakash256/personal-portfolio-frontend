0f63cdbb5d75063a00d6e1d67bbe14b515db96c4
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-10-28 23:55:47 -0400

    Client is being disconneted

--- src/app/demo/chatapp/page.tsx
+++ src/app/demo/chatapp/page.tsx
@@ -13,6 +13,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar, HeadingBar, CustomBody, SpaceBlock, Footer } from "front-end-component-kit";
 import { Container, Row, Col, Button, Form } from "react-bootstrap";
 import useWebSocket from 'react-use-websocket';
+import React from 'react';
 
 
 /*
@@ -29,6 +30,15 @@ recipient = "ben"
 */
 
 
+// The message interface
+interface Message {
+    messageid: number | null;
+    sender: string;
+    receiver: string;
+    message: string;
+    time: string;
+    }
+
 
 
 export default function TestChatService() {
@@ -37,6 +47,8 @@ export default function TestChatService() {
     const chatID = "qQyvvJ9s" ;
     const sender = "ben" ;
     const recipient = "ben" ; 
+
+
 
     const socketUrl = `ws://localhost:8050/chat-server/v1/ws/chat?chatID=${chatID}&sessionID=${sessionID}&user=${sender}`;
 
@@ -49,12 +61,26 @@ export default function TestChatService() {
     getWebSocket,
     } = useWebSocket(socketUrl, {
     onOpen: () => console.log('client is connected to server'),
+
+   
     //Will attempt to reconnect on all close events, such as server shutting down
     shouldReconnect: (closeEvent) => true,
     });
 
+
+    // Log any message received
+    React.useEffect(() => {
+        if (lastJsonMessage !== null) {
+        console.log('Received Object:', lastJsonMessage);
+        //console.log('Message' , lastJsonMessage.message) ;
+        //console.log('Sender' , lastJsonMessage.sender) ;
+        //console.log('Recipient' , lastJsonMessage.receiver) ;
+        //console.log('Time' , lastJsonMessage.time) ;
+        }
+    }, [lastJsonMessage]);
+
     return (
-        
+
     <div>
 
       <NavBar />
