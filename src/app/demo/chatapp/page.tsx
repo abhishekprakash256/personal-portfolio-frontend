ea79b920e284ff8f3e6855238dcb4457b6f9d6ed
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-10-28 22:47:03 -0400

    testing ws client

---
+++ src/app/demo/chatapp/page.tsx
@@ -0,0 +1,75 @@
+/*
+The testing of the websocket for the go server 
+using the react use websocket 
+
+can be accesed by /demo/chatapp
+*/
+
+"use client";
+
+
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavBar, HeadingBar, CustomBody, SpaceBlock, Footer } from "front-end-component-kit";
+import { Container, Row, Col, Button, Form } from "react-bootstrap";
+import useWebSocket from 'react-use-websocket';
+
+
+/*
+information for the connection to websocket 
+
+url - ws://localhost:8050/chat-server/v1/ws/chat?chatID=${chatID}&sessionID=${sessionID}&user=${sender}
+ChatID = "qQyvvJ9s"
+SessionID = "b14d0fe2-7dc7-4544-afd2-d292f48db712"
+sender = "bob"
+recipient = "ben"
+
+
+
+*/
+
+
+
+
+export default function TestChatService() {
+
+    const sessionID = "b14d0fe2-7dc7-4544-afd2-d292f48db712" ;
+    const chatID = "qQyvvJ9s" ;
+    const sender = "ben" ;
+    const recipient = "ben" ; 
+
+    const socketUrl = `ws://localhost:8050/chat-server/v1/ws/chat?chatID=${chatID}&sessionID=${sessionID}&user=${sender}`;
+
+    const {
+    sendMessage,
+    sendJsonMessage,
+    lastMessage,
+    lastJsonMessage,
+    readyState,
+    getWebSocket,
+    } = useWebSocket(socketUrl, {
+    onOpen: () => console.log('client is connected to server'),
+    //Will attempt to reconnect on all close events, such as server shutting down
+    shouldReconnect: (closeEvent) => true,
+    });
+
+    return (
+        
+    <div>
+
+      <NavBar />
+      <CustomBody>
+
+        <HeadingBar title="Testing the websokcet"></HeadingBar>
+
+        <SpaceBlock></SpaceBlock>
+      </CustomBody>
+
+
+
+      </div>
+
+    )
+
+}
+
